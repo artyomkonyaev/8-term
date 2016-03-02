85a6212196f94b63ffa478fb166122668c6df56f
codeserfer <admin@codeserfer.com>
2016-03-03 00:58:33 +0300

    Task #3 is completed

--- Development of distributed applications/3.cs
+++ Development of distributed applications/3.cs
@@ -10,37 +10,90 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
 
 namespace HW_3
 {
     class Program
     {
-        static System.Data.DataRowCollection GetValutesTable()
+        static DataRowCollection GetValutesTable(RBKServise.DailyInfo info)
         {
-            var info = new RBKServise.DailyInfo();
-
-            // Получение списка валют
-            // Справочник по кодам валют, содержит полный перечень валют котируемых Банком России.
-            // false - перечень ежедневных валют, true - перечень ежемесячных валют
             var DSC = info.EnumValutes(false);
 
             return DSC.Tables["EnumValutes"].Rows;
         }
 
+        static List<Decimal> GetValutesList(RBKServise.DailyInfo info, String currencySystemCode, DateTime fromDate, DateTime toDate)
+        {
+            var dataSet = info.GetCursDynamic(fromDate, toDate, currencySystemCode);
+
+            List<Decimal> valutesList = new List<decimal>();
+
+            for (int i = 0; i < dataSet.Tables.Count; i++)
+            {
+                DataTable thisTable = dataSet.Tables[i];
+
+                for (int j = 0; j < thisTable.Rows.Count; j++)
+                {
+                    DataRow row = thisTable.Rows[j];
+
+                    valutesList.Add(Decimal.Parse(row[3].ToString()));
+                }
+            }
+
+            return valutesList;
+        }
+
+        static String GetValuteSystemCode(DataRowCollection valutesTable, String currency)
+        {
+            foreach (DataRow row in valutesTable)
+            {
+                if (row.ItemArray[6].ToString().ToUpper() == currency.ToString().ToUpper())
+                {
+                    return row.ItemArray[0].ToString().Trim();
+                }
+            }
+
+            return "";
+        }
+
         static void Main(string[] args)
         {
-            foreach (System.Data.DataRow row in GetValutesTable())
+            try
             {
-                foreach (var item in row.ItemArray)
+                Console.WriteLine("Enter the currency code (for example, usd, eur): ");
+                String currencyCode = Console.ReadLine();
+
+                var info = new RBKServise.DailyInfo();
+
+                String currencySystemCode = GetValuteSystemCode(GetValutesTable(info), currencyCode);
+
+                if (String.IsNullOrEmpty(currencySystemCode))
                 {
-                    Console.Write("{0} ", item.ToString().Trim());
+                    throw new ArgumentException();
                 }
-                Console.WriteLine();
+
+
+                var latestDateTime = info.GetLatestDateTime();
+
+                var valutesList = GetValutesList(info, currencySystemCode, latestDateTime, latestDateTime.AddDays(1));
+
+                Console.WriteLine("The {0} exchange valutes against the ruble from {1:dd.mm.yyyy} to {2:dd.mm.yyyy}:", currencyCode, latestDateTime, latestDateTime.AddDays(1));
+
+                foreach (var valute in valutesList)
+                {
+                    Console.WriteLine(valute);
+                }
+
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Can't find this valute!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: {0}", e);
             }
         }
     }
 }
-
